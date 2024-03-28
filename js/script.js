9002799fe4e7d683e4adacdac616f4f9c039d870
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-03-28 18:51:19 +0000

    change the code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a trapezoid.
+ */
+function calculateAreaOfTrapezoid() {
+  // input
+  const baseaOfTrapezoid = parseInt(document.getElementById('base-a').value)
+  const basebOfTrapezoid = parseInt(document.getElementById('base-b').value)
+  const heightOfTrapezoid = parseInt(document.getElementById('height-of-trapezoid').value)
+
+  // process
+  const areaOfTrapezoid = (abaseOfTrapezoid + bbaseOfTrapezoid) / 2 * heightOfTrapezoid
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTrapezoid + ' mm²'
+}
